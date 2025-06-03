12fdd7e94d3882064e807d09c67dfde199c3f37f
rohansenturk <rohansenturk@gmail.com>
2025-06-03 22:16:03 +0300

    Add ngtp route with placeholder image

---
+++ app/ngtp/page.jsx
@@ -0,0 +1,14 @@
+export default function NgTp() {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <img src="/ngtp.jpg" alt="ngtp" style={{ maxWidth: '90%', height: 'auto' }} />
+    </div>
+  );
+}
